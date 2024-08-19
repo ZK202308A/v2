5d702af7aa12d3047627c5ca41e4d6ab696ba064
zk202308A <zk202308A@gmail.com>
2024-08-19 11:50:06 +0900

    vue123

---
+++ src/pages/SamplePage.vue
@@ -0,0 +1,29 @@
+<template>
+<MyNav></MyNav>
+<div class="container">
+
+    <MyTilte></MyTilte>
+  
+    <MyContent></MyContent>
+    
+    <hr>
+
+    <MyFooter></MyFooter>
+
+</div>
+</template>
+
+<script setup>
+import MyNav from '../components/MyNav.vue';
+import MyFooter from '../components/MyFooter.vue';
+import MyTilte from '../components/MyTilte.vue';
+import MyContent from '../components/MyContent.vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+
+
+
+</style>
