9c560534df2a5879e345aafdbcea88eb719b6827
zk202308A <zk202308A@gmail.com>
2024-08-20 15:30:59 +0900

    add1

--- src/components/ex4/TodoContainer.vue
+++ src/components/ex4/TodoContainer.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-    <TodoInput></TodoInput>
+    <TodoInput @addTodo="addTodo"></TodoInput>
     <TodoList></TodoList>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import TodoInput from './TodoInput.vue';
 import TodoList from './TodoList.vue';
+import { v4 as uuidv4 } from 'uuid';
+
+const todoList = ref([])
+
+const addTodo = (todo) => {
+  todo.tno = uuidv4()
+  todoList.value = [...todoList.value, todo]
+  
+  console.log(todoList.value)
+}
+
+
+
 </script>
 
 <style  scoped>
--- src/components/ex4/TodoInput.vue
+++ src/components/ex4/TodoInput.vue
@@ -1,12 +1,32 @@
 
 <template>
   <TodoBox :title="'Input New Todo'">
-    <input type="text">
+    <input type="text" v-model="todoRef.title">
+    <input type="text" v-model="todoRef.content">
+    <button @click="handleClick">ADD</button>
   </TodoBox>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import TodoBox from './TodoBox.vue';
+
+const initState = {title:'',content:''}
+
+const emits = defineEmits(['addTodo'])
+
+const todoRef = ref({...initState})
+
+const handleClick = () => {
+  console.log(todoRef.value)
+  const todoParam = {
+    title: todoRef.value.title,
+    content: todoRef.value.content
+  }
+  emits('addTodo', todoParam)
+}
+
+
 </script>
 
 <style lang="scss" scoped>
